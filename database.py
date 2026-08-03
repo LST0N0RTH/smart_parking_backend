667b5553537f38793276d4c23226e1f1ae357950
usashent <usashent@usashent-2.local>
2026-08-03 16:59:36 +0700

    เพิ่ม URL

--- database.py
+++ database.py
@@ -7,7 +7,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     DATABASE_URL = "postgresql://usashent@localhost/smart_parking"
 
-if DATABASE_URL.startswith("postgresql://"):
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+elif DATABASE_URL.startswith("postgresql://"):
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
 
 engine = create_engine(DATABASE_URL, echo=False)
